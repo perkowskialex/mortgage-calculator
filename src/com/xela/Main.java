1cf9a7c1291804ca47196a66df66c564c303b30f
Alex Perkowski <perkowskialex@gmail.com>
2020-03-30 15:06:00 -0500

    main calculator done, need to format currency

--- src/com/xela/Main.java
+++ src/com/xela/Main.java
@@ -1,8 +1,29 @@
 package com.xela;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner mortgage = new Scanner(System.in);
+        System.out.println("Enter Principal: ");
+        int principal = mortgage.nextInt();
+        System.out.println("Enter Annual Interest Rate: ");
+        float interestRate = mortgage.nextFloat();
+        // 100 percent, 12 months in a year
+        float monthlyInterest = interestRate / 100 / 12;
+        System.out.println("Enter period in years");
+        byte period = mortgage.nextByte();
+        int numberOfPayments = period * 12;
+
+        double result = principal*(monthlyInterest* Math.pow(1 +monthlyInterest, numberOfPayments))/(Math.pow(1+monthlyInterest, numberOfPayments) -1);
+
+
+        System.out.println("Principal: " + principal);
+        System.out.println("Annual Interest Rate: " + interestRate);
+        System.out.println("Period (Years): " + period);
+        System.out.println("Mortgage Payment: " + result);
     }
 }
